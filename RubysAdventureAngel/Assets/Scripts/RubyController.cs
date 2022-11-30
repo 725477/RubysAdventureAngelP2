10c24de0b946a26503dab6deef3e5f702f0571c3
725477 <116373882+725477@users.noreply.github.com>
2022-11-30 09:55:15 -0800

    Change the tile
    Changed the tile to none. Fixed ruby from going crazy.

--- RubysAdventureAngel/Assets/Scripts/RubyController.cs
+++ RubysAdventureAngel/Assets/Scripts/RubyController.cs
@@ -5,14 +5,26 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
+
     // Start is called before the first frame update
     void Start()
     {
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+    }
+     void FixedUpdate()
+    {
+   
+    
+    float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
         Vector2 position = transform.position;
         position.x = position.x + 3.0f * horizontal * Time.deltaTime;
