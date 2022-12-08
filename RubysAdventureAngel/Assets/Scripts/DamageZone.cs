460d2232da9f60fb070c4e9c0c8dee324c0fe52c
725477 <116373882+725477@users.noreply.github.com>
2022-12-08 09:58:15 -0800

    Finished code
    Finished code and move x and move y but I have an error

--- RubysAdventureAngel/Assets/Scripts/DamageZone.cs
+++ RubysAdventureAngel/Assets/Scripts/DamageZone.cs
@@ -4,10 +4,9 @@ using UnityEngine;
 
 public class DamageZone : MonoBehaviour
 {
-    // Start is called before the first frame update
     void OnTriggerEnter2D(Collider2D other)
     {
-        RubyController controller = other.GetComponent<RubyController>();
+        RubyController controller = other.GetComponent<RubyController >();
 
         if (controller != null)
         {
--- RubysAdventureAngel/Assets/Scripts/EnemyController.cs
+++ RubysAdventureAngel/Assets/Scripts/EnemyController.cs
@@ -12,11 +12,14 @@ public class EnemyController : MonoBehaviour
     float timer;
     int direction = 1;
 
+    Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
         timer = changeTime;
+        animator = GetComponent<Animator>();
     }
     void update()
     {
@@ -35,10 +38,14 @@ public class EnemyController : MonoBehaviour
         if (vertical)
         {
             position.y = position.y + Time.deltaTime * speed * direction; ;
+            animator.SetFloat("Move x", 0);
+            animator.SetFloat("Move Y",direction);
         }
         else
         {
             position.x = position.x + Time.deltaTime * speed * direction; ;
+            animator.SetFloat("Move X", direction);
+            animator.SetFloat("Move Y", 0);
         }
 
         rigidbody2D.MovePosition(position);
@@ -46,7 +53,7 @@ public class EnemyController : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D other)
     {
-        RubyController player = other.gameObject.GetComponent<RubyController>();
+        RubyController player = other.gameObject.GetComponent<RubyController >();
 
         if (player != null)
         {
