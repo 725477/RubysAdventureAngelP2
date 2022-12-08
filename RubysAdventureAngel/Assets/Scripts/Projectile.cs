8de0ac1a4ef9f988712f9f2bd9c8e1436695771e
725477 <116373882+725477@users.noreply.github.com>
2022-12-08 15:18:03 -0800

    Writing code
    I was writing code and added a cog. Also, got all of my project imported.

---
+++ RubysAdventureAngel/Assets/Scripts/Projectile.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    Rigidbody2D riigidbody2d;
+
+    void Awake()
+    {
+        rigidbody2D GetComponent<Rigidbody2D>();
+    }
+
+    public void Launch(Vector2 direction, float force)
+    {
+        rigidbody2d.Addforce(direction * force);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.magnitude > 1000.0f)
+        {
+            Destroy(gameObject)
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        EnemyController e = other.collider.GetComponent<EnemyController>();
+        if (e != null)
+        {
+            e.Fix();
+        }
+
+        Destroy(gameObject);
+    }
+}
